0cae4d282e0617ebf2512b4ca497e4010852d5dc
Kuba Bolek <jakubbolek02@gmail.com>
2024-04-14 19:43:01 +0200

    Basic CRUD and Code-First Migrations

--- Catering WebAPI/Data/DataContext.cs
+++ Catering WebAPI/Data/DataContext.cs
@@ -1,9 +1,16 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Catering_WebAPI.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace Catering_WebAPI.Data
 {
     public class DataContext : DbContext
     {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Customer> Customers { get; set; }
 
     }
 }
