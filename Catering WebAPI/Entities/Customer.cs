22d489df738c2a7fcc331cc146a0232cfa724508
Kuba Bolek <jakubbolek02@gmail.com>
2024-04-17 12:13:27 +0200

    Simple Code Refactoring with MaxLength and MinLength attributes.

--- Catering WebAPI/Entities/Customer.cs
+++ Catering WebAPI/Entities/Customer.cs
@@ -9,14 +9,13 @@ namespace Catering_WebAPI.Entities
     public class Customer
     {
         public int CustomerId { get; set; }
-        [MaxLength(20,ErrorMessage = "Name cannot be greater than 20 characters!")
-            ,MinLength(3,ErrorMessage = "Name cannot be less than 3 characters!")]
+        [MaxLength(20,ErrorMessage = "Name cannot be greater than 20 characters!")]
+        [MinLength(3,ErrorMessage = "Name cannot be less than 3 characters!")]
         public required string Name { get; set; }
-        [MaxLength(20, ErrorMessage = "Surname cannot be greater than 20 characters!")
-            , MinLength(3, ErrorMessage = "Surname cannot be less than 3 characters!")]
+        [MaxLength(20, ErrorMessage = "Surname cannot be greater than 20 characters!")]
+        [MinLength(3, ErrorMessage = "Surname cannot be less than 3 characters!")]
         public required string Surname { get; set; } 
         public string Email { get; set; } = string.Empty;
         public string Country { get; set; } = string.Empty;
-
     }
 }
